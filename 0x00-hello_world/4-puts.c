f1a09c400a1f4de547b78f54b1391b0cdf553c10
mamechera <cheradula999@gmail.com>
2023-02-16 07:12:50 -0800

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
+
 /**
  * main - display by using putst
  * returns 0
@@ -8,7 +9,7 @@
 int main(void)
 
 {
-	puts( "\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return(0);
 }

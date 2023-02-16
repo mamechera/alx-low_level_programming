49fe01f647083daa2b9655d6901f0a0072ad2fd8
mamechera <cheradula999@gmail.com>
2023-02-16 11:29:14 -0800

    printf file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
 
-
 /**
  * main - prints "programming is like building a multilingual
- * puzzle, followed by a newline."
- * returns 0
+ * puzzle", followed by a newline.
+ * Returns 0
+ *
  */
-
-/* main entry */
 
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }

31a5e727eb6d3105bd8899a6176b0d7f47ed8cfc
mamechera <cheradula999@gmail.com>
2023-02-16 06:58:57 -0800

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /*Entry point*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts( "\"Programming is like building a multilingual puzzle\n");
 	return(0);
 }

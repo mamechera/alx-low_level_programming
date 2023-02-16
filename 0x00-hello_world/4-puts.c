64f33c6ad91f88ff2cce85af8f7080aa01980b1f
mamechera <cheradula999@gmail.com>
2023-02-16 11:50:07 -0800

    printf file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,8 @@
 /**
  * main - Entry point prints "programming is like building a multilingual
  * puzzle", followed by a newline.
- * Returns 0.
- *
+ * Returns: 0 (Success)
  */
-
 int main(void)
 
 {

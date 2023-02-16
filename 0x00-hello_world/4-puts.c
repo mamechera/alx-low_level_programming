df82f879d3147815a477c92662471b691fda4214
mamechera <cheradula999@gmail.com>
2023-02-16 11:55:20 -0800

    printf file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
-
 /**
- * main - Entry point prints "programming is like building a multilingual
- puzzle", followed by a newline.
+ * main - Entry point
+ *
  * Returns: 0 (Success)
  */
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
 

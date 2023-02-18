1810f44c9a28bfc1100a27bd1e236440e9e31709
mamechera <cheradula999@gmail.com>
2023-02-17 22:24:17 -0800

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Return : Always 0 Success
+ * return: Always 0 Success
  */
-
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);

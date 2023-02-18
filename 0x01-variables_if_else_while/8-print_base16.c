bd7e792e227c33a4d21b76f84abb519a6cb6717b
mamechera <cheradula999@gmail.com>
2023-02-17 23:45:15 -0800

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:Always 0 Success
+ *
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch =< 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}

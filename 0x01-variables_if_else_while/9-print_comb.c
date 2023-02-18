44e8876e8d73dc86c7794b12bfae276ce903a826
mamechera <cheradula999@gmail.com>
2023-02-17 23:57:47 -0800

    combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:Always 0 Success
+ *
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		putchar(ch);
+		putchar(',');
+		putchar('\t');
+	}
+	putchar('\n');
+	return (0);
+}

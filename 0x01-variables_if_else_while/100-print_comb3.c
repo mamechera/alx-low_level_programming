371fc937e65b0361f8499fe43cec62e8ab70415c
mamechera <cheradula999@gmail.com>
2023-02-18 01:51:51 -0800

    combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:Always 0 Success
+ *
+ */
+
+int main(void)
+{
+    int ch;
+    int i;
+
+    for (ch = 48; ch < 57; ch++ )
+    
+    { 
+       
+        for (i = 49; i < 58; i++)
+        {
+            
+                
+            if (i>ch)
+	    {
+
+                
+                
+            putchar(ch);
+            if (i == 57  && ch == 56)
+	    {
+                
+                
+                    break;
+                }
+            putchar(i);
+            putchar(',');
+            putchar(' ');
+                
+            }
+            
+        }
+    }
+    
+    
+    putchar('\n');
+    return 0;
+}
+

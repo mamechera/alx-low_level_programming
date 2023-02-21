691272215ff228110c7e8cfb038ac4c51c5da4fc
mamechera <cheradula999@gmail.com>
2023-02-21 07:40:47 -0800

    an absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - function for absolute
+ *
+ * Return:always 0 success
+ *
+ */
+int _abs(int)
+{
+	int n;
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+		return (n);
+}

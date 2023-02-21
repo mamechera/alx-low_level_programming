6b6d6ed47b2a215f9f2122b7501d0662c7ed2a82
mamechera <cheradula999@gmail.com>
2023-02-21 05:43:46 -0800

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
-/** _islower - function which return lower number
+/** 
+ * _islower - function which return lower number
  *
  * Return:Always success
  *
  */
 int _islower(int c)
 {
-	if (c >= 97 && c<= 122)
+	if (c >= 97 && c <= 122)
 	{
-		return(1);
+		return (1);
 	}
 	return (0);
 }

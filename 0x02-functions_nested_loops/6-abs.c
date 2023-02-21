d0108d4a6279a18c6f6ec55e11b54b27bd7f9b26
mamechera <cheradula999@gmail.com>
2023-02-21 08:02:19 -0800

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 /**
  * _abs - function for absolute
- *
+ * @m: an arguement 
  * Return:always 0 success
  *
  */
@@ -12,7 +12,9 @@ int _abs(int m)
 	if (m < 0)
 	{
 		int absolute_value;
+
 		absolute_value = m * -1;
+
 		return (absolute_value);
 	}
 	return (m);

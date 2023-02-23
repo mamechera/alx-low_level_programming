5dc4d9fde030253e46b0e0992265c667e6ed96b9
mamechera <cheradula999@gmail.com>
2023-02-23 04:47:27 -0800

    digit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*print_most_numbers - it print more numbers
+ * 
+ * Return:always success
+ *
+ */
+void print_most_numbers(void)
+{
+	char ch;
+
+	for (ch = 0; ch <= 57; ch++)
+	{
+		if (ch != 50 && ch != 52)
+		{
+			_putchar(ch);
+		}
+	}
+	_putchar('\n');
+}

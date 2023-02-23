e222cd93a933ef5f6d2a8891d305882b42717620
mamechera <cheradula999@gmail.com>
2023-02-23 02:31:23 -0800

    an alphabet

--- 0x04-more-functions_nested_loops/0-isupper.c
+++ 0x04-more-functions_nested_loops/0-isupper.c
@@ -2,17 +2,18 @@
 
 /**
  * _isupper - its a function
- *
+ *@c:input arguiment
  * Retuns:always
  *
  */
 int _isupper(int c)
 {
-    if (c>=65 && c <= 90){
-        return (1);
+    if (c >= 65 && c <= 90)
+    {
+r	eturn (1);
     }
     else
     {
-    return (0);
+	return (0);
     }
 }

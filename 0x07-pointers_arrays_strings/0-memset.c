8f02c6baa4b1b5e3506072523f31634d6e315aad
mamechera <cheradula999@gmail.com>
2023-03-06 02:25:31 -0800

    memory assignment

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - memory assign function
+ * @s:input charchater
+ * @b:assign char
+ * @n:memory size
+ *
+ * Return:always success
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	for (i = 0; i < n; i++)
+	{
+		_putchar(s[i]);
+	}
+}
